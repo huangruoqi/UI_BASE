12e7879c2065437a064b1444d6edec098cd4fcbc
Ruoqi Huang <huangruoqi374@gmail.com>
2023-02-05 15:03:27 -0800

    add more components

---
+++ UI/components/color_bar.py
@@ -0,0 +1,27 @@
+from pixel_display import PixelDisplay
+import numpy
+
+class ColorBar(PixelDisplay):
+    colors = {
+        'black' : (0, 0, 0),
+        'red'   : (255, 100, 100),
+        'green' : (100, 255, 100),
+        'blue'  : (100, 255, 255),
+        'orange': (255, 255, 100),
+        'purple': (255, 100, 255),
+        'tint'  : (100, 255, 255),
+        'white' : (255, 255, 255),
+    }
+
+    def __init__(self, width, height, x, y):
+        if width % 100 != 0:
+            raise Exception("<width> for ColorBar must be multiples of 100")
+        super(ColorBar, self).__init__(width, height, x, y)
+        self.color_buffer = numpy.array([self.colors('black') for i in range(100)])
+        self.n = height
+
+    def set_color(self, i, color):
+        self.color_buffer[i] = self.colors[color]
+        np_arr = numpy.array([self.color_buffer for i in range(self.n)])
+        self.set(np_arr)
+
---
+++ UI/components/pixel_display.py
@@ -0,0 +1,12 @@
+from .container import Container
+import pygame
+from pygame.locals import *  # noqa
+
+
+class PixelDisplay(Container):
+    def __init__(self, width, height, x, y):
+        image=pygame.Surface([width, height])
+        super().__init__(image=image, align_mode="TOPLEFT",x=x,y=y)
+
+    def set(self, np_arr):
+        pygame.surfarray.blit_array(self.image, np_arr)
