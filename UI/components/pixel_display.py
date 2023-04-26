e04a3dd62a072de93a4aa78f690ffe814daa5d53
Ruoqi Huang <huangruoqi374@gmail.com>
2023-04-26 10:00:28 -0700

    add rotation

--- UI/components/pixel_display.py
+++ UI/components/pixel_display.py
@@ -1,5 +1,6 @@
 from .container import Container
 import pygame
+import numpy
 from pygame.locals import *  # noqa
 
 
@@ -8,7 +9,9 @@ class PixelDisplay(Container):
         super().__init__(x=x, y=y)
         image = pygame.Surface([width, height])
         pos = self.get_pos()
+        self.rot90 = 0
         self.set_image(image).set_pos(pos)
 
     def set(self, np_arr):
-        pygame.surfarray.blit_array(self.image, np_arr)
+        m = numpy.rot90(np_arr, k=self.rot90, axes=(0, 1))
+        pygame.surfarray.blit_array(self.image, m)
